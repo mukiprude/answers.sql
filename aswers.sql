b5878fd7938ce6c8a4f69638ca81c850bf706810
mukiprude <mukite18@gmail.com>
2025-08-13 12:03:48 +0300

    assignment

---
+++ aswers.sql
@@ -0,0 +1,25 @@
+-- Question 1
+select checkNumber, paymentDate, amount
+from payments;
+
+-- Question 2
+select orderDate, requiredDate, status
+from orders
+where status = 'In Process'
+order by orderDate desc;
+
+-- Question 3
+select firstName, lastName, email
+from employees
+where jobTitle = 'Sales Rep'
+order by employeeNumber desc;
+
+-- Question 4
+select *
+from offices;
+
+-- Question 5
+select productName, quantityInStock
+from products
+order by buyPrice asc
+limit 5;
