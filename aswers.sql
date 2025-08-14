6412966b6a9bb23fbb9c97c3f86d8194bad5fb7d
mukiprude <mukite18@gmail.com>
2025-08-14 12:26:46 +0300

    Update aswers.sql

--- aswers.sql
+++ aswers.sql
@@ -1,24 +1,24 @@
--- Question 1
+--  Question 1
 select checkNumber, paymentDate, amount
 from payments;
 
--- Question 2
+--  Question 2
 select orderDate, requiredDate, status
 from orders
 where status = 'In Process'
 order by orderDate desc;
 
--- Question 3
+--  Question 3
 select firstName, lastName, email
 from employees
 where jobTitle = 'Sales Rep'
 order by employeeNumber desc;
 
--- Question 4
+--  Question 4
 select *
 from offices;
 
--- Question 5
+--  Question 5
 select productName, quantityInStock
 from products
 order by buyPrice asc
